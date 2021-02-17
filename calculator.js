983c57a1cb24b0c3a7e04bfc3f8b3e58c8bbd152
thasty1707 <tim.hasty@gmail.com>
2021-02-17 10:27:51 -0600

    Add clear button; start javascript

--- calculator.js
+++ calculator.js
@@ -1,1 +1,9 @@
+let display = document.getElementById("calcDisplay")
+let numberButtons = document.getElementsByClassName("numberButtons");
+let mathButtons = document.getElementsByClassName("mathButtons");
+let clearDisplay = document.getElementById("clear");
+
+let numA = "";
+let numB = "";
+let calcResult = "";
 
