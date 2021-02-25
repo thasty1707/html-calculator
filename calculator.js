66047f81b033e48128e42d3128d903bc30675763
thasty1707 <tim.hasty@gmail.com>
2021-02-25 15:53:26 -0600

    Remove percent button and function, add backspace btn and fctn; working on operate

--- calculator.js
+++ calculator.js
@@ -1,26 +1,28 @@
 let display = document.getElementById("numbersDisplayed")
 let numberButtons = document.querySelectorAll(".numberButtons");
 let mathButtons = document.getElementsByClassName("mathButtons");
-let clearDisplay = document.getElementById("clear");
+let clearAll = document.getElementById("allClear");
+let clearNum = document.getElementById("clear");
 let equalButton = document.getElementById("equals");
 
 let numA = "";
 let numB = "";
+let numC = "";
 let mathOperator = "";
 
 //function to choose the numbers
 function chooseNumbers(e){
     if(mathOperator === ""){
         if(numA.length < 10){
+          //Add digit or decimal to end of numA; replace numA with new digit if numA == 0
             switch(e.currentTarget.dataset.number){
-                //Add chosen digit or decimal to end of numA; replace numA with new digit if numA == 0
                 case "one":
-                    if(numA !== "" && numA != "0"){
+                    if(numA !== "" && numA !== "0"){
                         numA += 1;
                     }else{
-                        numA = "1";
-                    }
-                   break;
+                        numA = "1"
+                    };
+                    break;
                 case "two":
                     if(numA !== "" && numA !== "0"){
                         numA += 2;
@@ -166,10 +168,10 @@ function chooseNumbers(e){
                     };
                     break;
                 case "zero":
-                    if(numB !== ""){
+                    if(numB !== "" && numB !== "0"){
                         numB += 0;
                     }else {
-                        numB = ""
+                        numB = "0"
                     };
                     break;
                 case "decimal":
@@ -187,11 +189,12 @@ function chooseNumbers(e){
                 return;
         };
     };
+    console.log("numA=",numA, "numB=",numB, "numC=",numC);
 };
 
 //function to choose addition, subtraction, multiplication, or division
 function chooseOperator(e){
-    if(numB == ""){
+    if(mathOperator == ""){
         switch(e.currentTarget.dataset.operators){
             case "plus":
                 mathOperator = "add"
@@ -213,11 +216,13 @@ function chooseOperator(e){
 
 //function to execute operation
 function operate(){
-    if(mathOperator != "" && numB != ""){
+    if(numB === 0 && mathOperator === "divide"){
+        alert("Cannot divide by 0\! Please try again\.")
+    }else if(numA != "" && numB != "" && mathOperator != ""){
         switch(mathOperator){
             case 'add':
-            numC = parseFloat(numA) + parseFloat(numB);
-            break;
+                numC = parseFloat(numA) + parseFloat(numB);
+                break;
             case 'subtract':
                 numC = numA - numB;
                 break;
@@ -228,18 +233,38 @@ function operate(){
                 numC = numA/numB;
                 break;
         };
+        mathOperator = "";
+    }else if(numA == "" && numC != "" && numB != "" && mathOperator != ""){
+        switch(mathOperator){
+            case 'add':
+                numC += parseFloat(numB);
+                break;
+            case 'subtract':
+                numC -= numB;
+                break;
+            case 'multiply':
+                numC *= numB;
+                break;
+            case 'divide':
+                numC /= numB;
+                break;
+        };
+        mathOperator = "";
     }else{
         return;
     };
     stringC = numC.toString();
-    if(stringC.length > 9){
-        numA = stringC.slice(0, 8);
+
+    //Reduce the length of the number to fit the display area
+    if(stringC.length > 10){
+        numC = stringC.slice(0, 9);
     }else{
-        numA = numC;
-    };
+        numC = Number(stringC);
+    }
+    display.innerHTML = numC;
+    numA = "";    
     numB = "";
     mathOperator = "";
-    display.innerHTML = numA;
 };
 
 //function to reset calculator
@@ -250,9 +275,23 @@ function clearCalculator(){
     display.innerHTML = "";
 };
 
+//function to clear last number entered
+function clearEntry(){
+    if(display.innerHTML == numA){
+        numA = "";
+        display.innerHTML = numA;
+    }else if(display.innerHTML == numB){
+        numB = "";
+        display.innerHTML = numB;
+    }else{
+        numC = "";
+        display.innerHTML = numC;
+    };
+};
+
 //function to make numbers positive or negative
 function positiveOrNegative(){
-    if(mathOperator === ""){
+    if(display.innerHTML === numA){
         if(numA >= 0){
             numA = "\-" + numA;
             display.innerHTML = numA;
@@ -260,22 +299,51 @@ function positiveOrNegative(){
             numA = numA.slice(1,9);
             display.innerHTML = numA;
         };
-    }else if(mathOperator !== ""){
+    }else if(display.innerHTML === numB){
         if(numB >= 0){
             numB = "\-" + numB;
             display.innerHTML = numB;
         }else{
-            //
             numB = numB.slice(1,9);
             display.innerHTML = numB;
+        };
+    }else if (display.innerHTML === numC){
+        if(numC >= 0){
+            numC = "\-" + numC;
+            display.innerHTML = numC;
+        }else{
+            numC = numC.slice(1,9);
+            display.innerHTML = numC;
         };
     }else{
         return;
     };
 };
 
+//function to delete entry 1 digit at a time
+function backspace(){
+    if(mathOperator !== ""){
+        mathOperator = "";
+    }else if(display.innerHTML == numA){
+        let numStr = numA.toString();
+        numA = numStr.slice(0, numStr.length-1);
+        display.innerHTML = numA;    
+    }else if(display.innerHTML == numB){
+        let numStr = numB.toString();
+        numB = numStr.slice(0, numStr.length-1);
+        display.innerHTML = numB;    
+    }else if(display.innerHTML == numC){
+        let numStr = numC.toString();
+        numC = numStr.slice(0, numStr.length-1);
+        display.innerHTML = numC;   
+    }else{
+        return;
+    };
+};
+
 numberButtons.forEach(numberButton => numberButton.addEventListener('click', chooseNumbers));
-clearDisplay.addEventListener('click',clearCalculator);
+clearAll.addEventListener('click',clearCalculator);
+clearNum.addEventListener('click',clearEntry);
 equalButton.addEventListener('click',operate);
 
 let add = document.getElementById("plus").addEventListener('click',chooseOperator);
@@ -283,3 +351,4 @@ let subtract = document.getElementById("minus").addEventListener('click',chooseO
 let multiply = document.getElementById("times").addEventListener('click',chooseOperator);
 let divide = document.getElementById("dividedBy").addEventListener('click',chooseOperator);
 let posNeg = document.getElementById("positiveNegative").addEventListener('click',positiveOrNegative);
+let correction = document.getElementById("backspace").addEventListener('click',backspace);
